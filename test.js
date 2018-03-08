0c413387432a82232010960eb9c065049b900d35
jneidel <jneidel@outlook.com>
2018-03-08 18:10:57 +0100

    Update tests/travis config, add todo files

--- test.js
+++ test.js
@@ -4,13 +4,24 @@ const helpers = require( "yeoman-test" );
 
 /* globals describe it beforeEach */
 
-describe( "gerator-boilerplate:app", () => {
-  beforeEach( () => helpers
-    .run( path.join( __dirname, "./app" ) )
-    .withPrompts( { someAnswer: true } ) );
+describe( "generator-jneidel", () => {
+  describe( "jneidel:node", () => {
+    beforeEach( () => helpers
+      .run( path.join( __dirname, "./generators/app" ) )
+      .withPrompts( { someAnswer: true } ) );
 
-  it( "creates files", () => {
-    assert.file( [ ".travis.yml", ".gitignore", ".npmrc", "index.js", "licence", "test.js", "readme.md", "package.json" ] );
+    it( "creates files", () => {
+      assert.file( [ ".travis.yml", ".gitignore", ".npmrc", ".editorconfig", "index.js", "licence", "test.js", "readme.md", "package.json", "todo.md" ] );
+    } );
+  } );
+
+  describe( "jneidel:chrome", () => {
+    beforeEach( () => helpers
+      .run( path.join( __dirname, "./generators/chrome" ) )
+      .withPrompts( { someAnswer: true } ) );
+
+    it( "creates files", () => {
+      assert.file( [ ".travis.yml", ".gitignore", ".npmrc", ".editorconfig", "webpack.config.js", "licence", "vars.env", "readme.md", "package.json", "todo.md", "dist/img/icon.png", "src/js/background.js", "src/js/options.js", "src/scss/options.scss", "src/bundles/options.bundle.js", "src/pug/options.pug", "tests/test.js", "dist/manifest.json" ] );
+    } );
   } );
 } );
-
