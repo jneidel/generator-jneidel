14d9a3f87ce21848a31f37c16fb97f2bb6aa185f
jneidel <jneidel@outlook.com>
2018-03-25 23:26:09 +0200

    Add ava config, rename 'tests'->'test'

--- generators/chrome/index.js
+++ generators/chrome/index.js
@@ -1,10 +1,10 @@
 const genGenerator = require( "../generatorFactory" );
 
 module.exports = genGenerator( { long: "chrome extension", short: "extension" }, {
-  folders   : [ "dist", "dist/img", "src", "src/js", "src/scss", "src/pug", "src/bundles", "tests" ],
+  folders   : [ "dist", "dist/img", "src", "src/js", "src/scss", "src/pug", "src/bundles", "test" ],
   dotfiles  : [ "gitignore", "npmrc", "editorconfig" ],
   files     : [ "webpack.config.js", "todo.md", "vars.env", "dist/img/icon.png", "src/js/background.js", "src/js/options.js", "src/scss/options.scss", "src/bundles/options.bundle.js" ],
-  insertName: [ "src/pug/options.pug", "tests/test.test.js" ],
+  insertName: [ "src/pug/options.pug", "test/test.test.js" ],
   insert    : [
     [ "licence", [ "authorName", "authorEmail", "authorUrl", "year" ] ],
     [ "readme.md", [ "moduleName", "authorName", "authorUrl", "username", "description", "codestyle" ] ],
--- generators/web/index.js
+++ generators/web/index.js
@@ -1,10 +1,10 @@
 const genGenerator = require( "../generatorFactory" );
 
 module.exports = genGenerator( { long: "node webapp", short: "app" }, {
-  folders   : [ "controllers", "public", "public/img", "models", "routes", "src", "src/bundles", "src/js", "src/pug", "src/scss", "tests", "tests/unit", "tests/integration", "logs", "handlers" ],
+  folders   : [ "controllers", "public", "public/img", "models", "routes", "src", "src/bundles", "src/js", "src/pug", "src/scss", "test", "test/unit", "test/integration", "logs", "handlers" ],
   dotfiles  : [ "gitignore", "npmrc", "editorconfig" ],
-  files     : [ "models/reserved-usernames.js", "models/User.js", "models/passport.js", "routes/api.js", "routes/auth.js", "src/bundles/welcome.bundle.js", "src/js/welcome.js", "src/scss/welcome.scss", "src/scss/_vars.scss", "src/scss/layout.scss", "src/pug/login.pug", "src/pug/register.pug", "tests/unit/utils.js", "webpack.config.js", "todo.md", "app.js", "logs/utils.js", "logs/logger.js", "public/img/favicon.ico", "handlers/errorHandlers.js", "src/pug/error.pug" ],
-  insertName: [ "routes/index.js", "src/pug/layout.pug", "src/pug/welcome.pug", "tests/unit/test.test.js", "vars.env" ],
+  files     : [ "models/reserved-usernames.js", "models/User.js", "models/passport.js", "routes/api.js", "routes/auth.js", "src/bundles/welcome.bundle.js", "src/js/welcome.js", "src/scss/welcome.scss", "src/scss/_vars.scss", "src/scss/layout.scss", "src/pug/login.pug", "src/pug/register.pug", "test/unit/utils.js", "webpack.config.js", "todo.md", "app.js", "logs/utils.js", "logs/logger.js", "public/img/favicon.ico", "handlers/errorHandlers.js", "src/pug/error.pug" ],
+  insertName: [ "routes/index.js", "src/pug/layout.pug", "src/pug/welcome.pug", "test/unit/test.test.js", "vars.env" ],
   insert    : [
     [ "licence", [ "authorName", "authorEmail", "authorUrl", "year" ] ],
     [ "readme.md", [ "moduleName", "authorName", "authorUrl", "username", "description", "codestyle" ] ] ],
