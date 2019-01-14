a24df4e1fd26e91e971f8e76c3f233c7821980d1
Jonathan Neidel <github@jneidel.com>
2019-01-14 21:21:52 +0100

    Fix env.example

--- generators/web/index.js
+++ generators/web/index.js
@@ -2,9 +2,9 @@ const genGenerator = require( "../generator-factory" );
 
 module.exports = genGenerator( { long: "node webapp", short: "app" }, {
   folders   : [ "controllers", "public", "models", "routes", "src", "src/bundles", "src/js", "src/pug", "src/scss", "test", "test/unit", "test/integration", "handlers", "util", "logs" ],
-  dotfiles  : [ "gitignore" ],
+  dotfiles  : [ "gitignore", "env.example" ],
   files     : [ "models/reserved-usernames.js", "models/User.js", "models/passport.js", "routes/api.js", "routes/auth.js", "src/bundles/welcome.bundle.js", "src/js/welcome.js", "src/scss/welcome.scss", "src/scss/_vars.scss", "src/scss/layout.scss", "src/pug/login.pug", "src/pug/register.pug", "test/unit/utils.js", "webpack.config.js", "app.js", "public/favicon.ico", "handlers/errorHandlers.js", "src/pug/error.pug", "util/secrets.js", "logs/http-logger.js", "logs/utils.js" ],
-  insertName: [ "routes/index.js", "src/pug/layout.pug", "src/pug/welcome.pug", "test/unit/test.test.js", ".env.example" ],
+  insertName: [ "routes/index.js", "src/pug/layout.pug", "src/pug/welcome.pug", "test/unit/test.test.js" ],
   insert    : [
     [ "readme.md", [ "moduleName", "authorName", "authorUrl", "username", "description", "codestyle" ] ],
   ],
